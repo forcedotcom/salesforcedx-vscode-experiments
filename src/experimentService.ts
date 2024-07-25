9532226808066dc20447611753bf7b38393cfc0d
Jon Gellin <jgellin@salesforce.com>
2024-07-25 11:03:27 -0400

    feat: experiment service initial commit

---
+++ src/experimentService.ts
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2024, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ **/
+
+import * as vscode from 'vscode';
+import { Experiment, IExperimentService } from './api';
+
+class ExperimentService implements IExperimentService {
+    private experiments: Experiment[] = [];
+
+    private static instance: ExperimentService;
+
+    public static getInstance(): ExperimentService {
+        if (!ExperimentService.instance) {
+            ExperimentService.instance = new ExperimentService();
+        }
+
+        return ExperimentService.instance;
+    }
+
+    private constructor() {}
+
+    registerExperiments(context: vscode.ExtensionContext, experiments: Experiment[]): void {
+        this.experiments = experiments;
+    }
+
+    getExperiments(): Experiment[] {
+        return this.experiments;
+    }
+}
+
+export function getExperimentService(): IExperimentService {
+    return ExperimentService.getInstance();
+}
---
+++ test/unit/experimentService.test.ts
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2024, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ **/
+
+import * as vscode from 'vscode';
+import { ExperimentType, getExperimentService } from '../../src';
+
+describe('ExperimentService', () => {
+    it('should register and return experiments', () => {
+        const experiments = [
+            {
+                id: '1',
+                name: 'Experiment1',
+                type: ExperimentType.Stateful,
+                distributionPercent: 50,
+            },
+            {
+                id: '2',
+                name: 'Experiment2',
+                type: ExperimentType.Transactional,
+                distributionPercent: 50,
+            },
+        ];
+        const experimentService = getExperimentService();
+        experimentService.registerExperiments({} as vscode.ExtensionContext, experiments);
+        expect(experimentService.getExperiments()).toEqual(experiments);
+    });
+});
